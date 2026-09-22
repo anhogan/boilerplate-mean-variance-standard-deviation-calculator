68d79c35fa25ad40b0780b1556e45958c9dd6abf
Amanda McQueen <40039041+anhogan@users.noreply.github.com>
2026-09-22 16:18:15 +0000

    Solution to mean_var_std.py file

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,21 @@
 import numpy as np
 
+def mathEquations(matrix, equation):
+    np_func = getattr(np, equation)
+    return [np_func(matrix, axis=0).tolist(), np_func(matrix, axis=1).tolist(), np_func(matrix).item()]
+
 def calculate(list):
+    if len(list) == 9:
+        matrix = np.array(list).reshape(3, 3)
+        calculations = {
+            'mean': mathEquations(matrix, "mean"),
+            'variance': mathEquations(matrix, "var"),
+            'standard deviation': mathEquations(matrix, "std"),
+            'max': mathEquations(matrix, "max"),
+            'min': mathEquations(matrix, "min"),
+            'sum': mathEquations(matrix, "sum")
+        }
 
-
-
-
-    return calculations
+        return calculations
+    else:
+        raise ValueError('List must contain nine numbers.')
